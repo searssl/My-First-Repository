ab40dd2a1fe72382a8c7d825c7ae9d8c8e712b8a
searssl <55798257+searssl@users.noreply.github.com>
2019-10-30 16:54:55 -0400

    Added W7 Exercise

---
+++ Week 7/ROICalc/Raw/ROICalc.R
@@ -0,0 +1,13 @@
+#' ROICalc: A package with a simple ROI Calculator.
+#'
+#' This package is a calculator for determining the return
+#' on an investment, based off of the intial investment,
+#' share purchase and sell price, and the broker fees. An
+#' example dataset is included highlighting a series of
+#' ROI outputs at different sell values.
+#'
+#' @docType package
+#' @name ROICalc
+#' @author Sean Sears, McMaster University \email{searssl@@mcmaster.ca}
+#' @references \url{https://www.investopedia.com/terms/r/returnoninvestment.asp}
+NULL
